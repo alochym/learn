5f8bcda1a4db42d6e70918fc5ca1df35e58710ed
Do Nguyen Ha <donguyenha@live.com>
2023-03-18 00:57:16 +0700

    write basic service for testing

--- Kubernetes/code/golang/ultimate-service/main.go
+++ Kubernetes/code/golang/ultimate-service/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var build = "development"
 
 func main() {
-	fmt.Println("Hello")
+	log.Println("Starting service", build)
+
+	defer log.Println("service end")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	<-shutdown
+
+	log.Println("Stopping service")
 }
